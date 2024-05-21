84673ad1e44b45022543a4d29b46531487b34a1a
Kees Kuip <keeskuip@cubeautomatisering.nl>
2024-05-21 13:26:10 +0200

    module-info adjustments

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
-module materialdesignicons
+module org.kku.materialdesignicons
 {
   requires java.logging;
   requires transitive javafx.graphics;
   requires transitive javafx.controls;
-  requires com.ibm.icu;
+  requires static com.ibm.icu;
 
   exports org.kku.fonticons.ui;
   exports org.kku.fonticons.sample;
